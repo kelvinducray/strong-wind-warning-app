949417a8d91950d4585d1fbea3350bc587f17b79
Kelvin Ducray <kelvinducray@192-168-1-101.tpgi.com.au>
2021-10-09 21:35:21 +1100

    Added more logic, unit tests and in-code documentation

--- strong_wind_warning_app/thresholds.py
+++ strong_wind_warning_app/thresholds.py
@@ -1,4 +1,11 @@
-def _get_kph(speed_mps):
+from numbers import Number
+
+
+def _get_kmph(speed_mps: Number):
+    """
+    Converts speeds from metres/second to kilometers per hour.
+    """
+
     if speed_mps >= 0:
         speed_kmph = speed_mps * 3.6
         return speed_kmph
@@ -6,12 +13,97 @@ def _get_kph(speed_mps):
         raise ValueError("Input speed (m/s) must be a postive number.")
 
 
-# def get_wind_status(wind_forecast: dict):
+def _get_direction_name(deg: Number):
+    """
+    Note: In this implementation on the borderlines of NE, SE, SW, & NW, the
+    respective N, E, S, W direction will be prioritised.
 
-#     # {'wind_speed': 2.24, 'wind_deg': 185, 'wind_gust': 4.02}
+    E.g. If the angle is precisely 22.5 degrees (the boundary between N & NE),
+    N will be preferentially chosen.
+    """
 
-#     speed = wind_forecast['wind_speed']
-#     deg = wind_forecast['wind_deg']
-#     gust = wind_forecast['wind_gust']
+    # Check validity of angle first
+    if (deg <= 360) and (deg >= 0):
+        # North
+        if (deg >= 337.5) or (deg <= 22.5):
+            return "Notherly"
+        # East
+        elif (deg >= 67.5) and (deg <= 112.5):
+            return "Easterly"
+        # South
+        elif (deg >= 157.5) and (deg <= 202.5):
+            return "Southerly"
+        # West
+        elif (deg >= 247.5) and (deg <= 292.5):
+            return "Westerly"
+        # NE
+        elif (deg > 22.5) and (deg < 67.5):
+            return "North Easterly"
+        # SE
+        elif (deg > 112.5) and (deg < 157.7):
+            return "South Easterly"
+        # SW
+        elif (deg > 202.5) and (deg < 247.5):
+            return "South Westerly"
+        # NW
+        elif (deg > 292.5) and (deg < 337.5):
+            return "North Westerly"
+    else:
+        raise ValueError("Input degrees must be a between 0 and 360.")
 
-#     if (speed >= 3) or (gust >= 4):
+
+def get_wind_status(wind_forecast: dict):
+
+    """
+    This function ingests the filtered response from the weather API and
+    determines the wind risk. It returns a dictionary with a status message,
+    what the wind forecast is and free text to paddlers advising what to do
+    given the forecast data.
+
+    Input dictionary format example:
+    {
+        'wind_speed': 2.24,
+        'wind_deg': 185,
+        'wind_gust': 4.02
+    }
+
+    Where the wind speeds are expressed in metres/second and the angle is the
+    direction where the wind is coming from (North being zero degrees and the
+    angles increading in the clockwise direction).
+    """
+
+    speed = _get_kmph(wind_forecast["wind_speed"])
+    gust = _get_kmph(wind_forecast["wind_gust"])
+    direction = _get_direction_name(wind_forecast["wind_deg"])
+
+    # Caution warning
+    if (speed >= 30) or (gust >= 40):
+        return {
+            "status": "Caution",
+            "wind_forecast": ("" ""),
+            "message": (
+                f"Strong {direction} winds. In-experienced paddlers please "
+                "consider cancelling your session. Experienced paddlers "
+                "please exercise extreme caution."
+            ),
+        }
+    # Cancel classes
+    elif (speed >= 40) or (gust >= 50):
+        return {
+            "status": "Hazardous condtions",
+            "wind_forecast": ("" ""),
+            "message": (
+                f"Very strong {direction} winds. All paddlers please cancel "
+                "your session today."
+            ),
+        }
+    # No warnings
+    else:
+        return {
+            "status": "No current warnings",
+            "wind_forecast": ("" ""),
+            "message": (
+                "No current wind warnings, however please still paddle with "
+                "caution and stick together."
+            ),
+        }
--- tests/test_thresholds.py
+++ tests/test_thresholds.py
@@ -2,7 +2,10 @@ from numbers import Number
 from typing import Any
 
 import pytest
-from strong_wind_warning_app.thresholds import _get_kph  # , get_wind_status
+from strong_wind_warning_app.thresholds import (  # , get_wind_status
+    _get_direction_name,
+    _get_kmph,
+)
 
 
 # Tests for _get_kph()
@@ -11,7 +14,7 @@ from strong_wind_warning_app.thresholds import _get_kph  # , get_wind_status
     [(0, 0), (5, 18), (1.2334343, 4.44036348)],
 )
 def test_get_kph(input: Number, expected: Number) -> None:
-    actual = _get_kph(input)
+    actual = _get_kmph(input)
     assert actual == pytest.approx(expected)
 
 
@@ -19,18 +22,109 @@ def test_get_kph(input: Number, expected: Number) -> None:
     "input",
     [-4, -2.7853434, -3.1],
 )
-def test_get_kph_not_positive(input: Any):
+def test_get_kph_not_positive(input: Any) -> None:
     with pytest.raises(ValueError):
-        _get_kph(input)
+        _get_kmph(input)
 
 
 @pytest.mark.parametrize(
     "input",
     ["str", None, {}, []],
 )
-def test_get_kph_not_numeric(input: Any):
+def test_get_kph_not_numeric(input: Any) -> None:
     with pytest.raises(TypeError):
-        _get_kph(input)
+        _get_kmph(input)
+
+
+# Tests for _get_direction_name()
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        # North
+        (355, "N"),
+        (0, "N"),
+        (360, "N"),
+        (16.7, "N"),
+        # East
+        (82, "E"),
+        (90, "E"),
+        (91.34, "E"),
+        # South
+        (170, "S"),
+        (180, "S"),
+        (190.01, "S"),
+        # West
+        (263, "W"),
+        (270, "W"),
+        (281.04, "W"),
+        # NE
+        (30.02, "NE"),
+        (45, "NE"),
+        (51, "NE"),
+        # SE
+        (130.01, "SE"),
+        (135, "SE"),
+        (141.93, "SE"),
+        # SW
+        (209.443, "SW"),
+        (225, "SW"),
+        (231, "SW"),
+        # NW
+        (300, "NW"),
+        (315, "NW"),
+        (322.6, "NW"),
+    ],
+)
+def test_get_direction_name(input: Number, expected: Number) -> None:
+    actual = _get_direction_name(input)
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "input, expected",
+    [
+        # North/NE Boundary
+        (22.5, "N"),
+        # East/NE Boundary
+        (67.5, "E"),
+        # East/SE Boundary
+        (112.5, "E"),
+        # South/SE Boundary
+        (157.5, "S"),
+        # South/SW Boundary
+        (202.5, "S"),
+        # West/SW Boundary
+        (247.5, "W"),
+        # West/NW Boundary
+        (292.5, "W"),
+        # North/NW Boundary
+        (337.5, "N"),
+    ],
+)
+def test_get_direction_name_on_boundaries(
+    input: Number,
+    expected: str,
+) -> None:
+    actual = _get_direction_name(input)
+    assert actual == expected
+
+
+@pytest.mark.parametrize(
+    "input",
+    [-100, -0.0001, 360.0001, 720],
+)
+def test_get_get_direction_name_range(input: int) -> None:
+    with pytest.raises(ValueError):
+        _get_direction_name(input)
+
+
+@pytest.mark.parametrize(
+    "input",
+    ["str", None, {}, []],
+)
+def test_get_direction_name_not_numeric(input: Any) -> None:
+    with pytest.raises(TypeError):
+        _get_direction_name(input)
 
 
 # Tests for get_wind_status()
